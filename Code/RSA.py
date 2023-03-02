cf326c542afcbc7239e2b056b07f16fd803671db
Lemon9247 <87786699+Lemon9247@users.noreply.github.com>
2023-03-02 01:58:32 +0000

    Forgot to commit RSA tweaks

--- Code/RSA.py
+++ Code/RSA.py
@@ -1,21 +1,15 @@
 import random
 import math
 
-
 def inverse(a, m):  # Gives the modular inverse of a modulo m
     for b in range(1, m):
         if (a * b) % m == 1:
             return b
     return -1
 
-
-p = random.randint(1, 1000)
-q = random.randint(1, 1000)  # Two random large integers p and q for key generation
-
-
-def keys(p, q, key_size):
-    min = 1 * 2 ** (key_size - 1)
-    max = (1 * 2 ** key_size) - 1
+def keys(key_size):
+    min = 2 ** (key_size - 1)
+    max = (2 ** key_size) - 1
     primes = [2]
     start = 1 * 2 ** (key_size // 2 - 1)
     stop = 1 * 2 ** (key_size // 2 + 1)
@@ -36,7 +30,6 @@ def keys(p, q, key_size):
         if q_values:
             q = random.choice(q_values)
             break
-    print(p, q)
     n = p * q
     s = (p - 1) * (q - 1)
     while True:
@@ -53,28 +46,34 @@ def encrypt(message, package):
     cipher = [pow(ord(c), e, n) for c in message]
     return cipher
 
-
 def decrypt(cipher, package):
     d, n = package
     message = [chr(pow(c, d, n)) for c in cipher]
     return ''.join(message)
 
 
-if __name__ == "__main__":
+def main():
+    # Two random large integers p and q for key generation
     bit_length = int(input("Enter bit_length: "))
     while bit_length <= 2:
         print("Invalid bit_length, has to be greater than two")
         bit_length = int(input("Enter new bit_length greater than two: "))
+
     print("Running RSA...")
     print("Generating public and private keys...")
-    public, private = keys(
-        p, q, 2 ** bit_length)
+    public, private = keys(2 ** bit_length)
     print("Public Key: ", public)
     print("Private Key: ", private)
+
     msg = input("Write message: ")
     print([ord(c) for c in msg])
+
     encrypted_msg = encrypt(msg, public)
     print("Encrypted message: ")
-    print(''.join(map(lambda x: str(x), encrypted_msg)))
+    print(encrypted_msg)
     print("Decrypted message: ")
     print(decrypt(encrypted_msg, private))
+
+
+if __name__ == "__main__":
+    main()
